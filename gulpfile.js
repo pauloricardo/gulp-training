9e275838b1dd92d33410392985f0ca05b312e624
Monteiro <paulo.r.monteiro@accenture.com>
2017-01-16 13:43:23 -0300

    Terceira aula

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
-var gulp = require('gulp'),imagemin = require('gulp-imagemin'), clean = require('gulp-clean');
+var gulp = require('gulp'),imagemin = require('gulp-imagemin'), clean = require('gulp-clean'),
+concat = require('gulp-concat'),uglify = require('gulp-uglify'),usemin = require('gulp-usemin'), 
+rename = require('gulp-rename'), rev = require('gulp-rev');
 
+gulp.task('default', ['copy'], function(){
+    gulp.start('usemin');
+});
 gulp.task('copy', ['clean'], function() {
     gulp.src('src/**/*')
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-img', ['copy'], function(){
+gulp.task('build-img', function(){
     gulp.src('dist/img/*').pipe(imagemin()).pipe(gulp.dest('dist/img'));
 });
 
@@ -13,5 +18,11 @@ gulp.task('build-img', ['copy'], function(){
 gulp.task('clean', function() {
      var stream = gulp.src('dist')
         .pipe(clean());
+    return stream;
+});
+gulp.task('usemin', function() {
+ var stream = gulp.src('classes/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('dist/'));
     return stream;
 });
